dd180836c2cca7010f38c41625ecc3d25f9b3c31
merve <merve.nur14@outlook.com>
2022-07-15 17:50:35 +0300

    Add project files.

---
+++ OOP_1.ders/Ev.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_1.ders
+{
+    public class Ev
+    {
+        private int odaSayisi;
+        private string adres;
+
+        public int getodaSayisi() 
+        {
+            return odaSayisi;
+        }
+
+        public void setodaSayisi(int odaSayisi)
+        {
+            if (odaSayisi > 8)
+            {
+                Console.WriteLine("Oda Sayısı 8'den büyük olamaz");
+            }
+            else
+            {
+                this.odaSayisi = odaSayisi;
+            }
+        }
+
+        public string getadres()
+        {
+            return adres;
+        }
+        public void setadres(string adres)
+        {
+            this.adres = adres;
+        }
+        
+
+    }
+}
---
+++ OOP_1.ders/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_1.ders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ev ev = new Ev();
+            ev.setodaSayisi(5);
+            Console.WriteLine("Oda Sayısı: {0}", ev.getodaSayisi());
+            
+            ev.setadres("istanbul");
+            Console.WriteLine("Adres: {0}", ev.getadres());
+
+        }
+    }
+}
